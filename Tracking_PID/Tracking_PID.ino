7db54f58a48326a981001dcc85cf2b75f7335a70
五丝菜卷 <chenjiegd@foxmail.com>
2019-07-30 17:35:04 +0800

    资料保存

--- Tracking_PID/Tracking_PID.ino
+++ Tracking_PID/Tracking_PID.ino
@@ -31,13 +31,134 @@ void setup()
 	pinMode(A2, INPUT);
 }
 
+int pos;
 void loop()
 {
 	// put your main code here, to run repeatedly:
+	pos = echoTrace();
+  	stateMachine(pos);
+}
+
+//PID算法部分
+float Kp = 120; // 25
+float Ki = 0;   // 0.15
+float Kd = 0;   //1200
+float error, errorLast, erroInte;
+float calcPid(float input)
+{
+	float errorDiff;
+	float output;
+	error = error * 0.7 + input * 0.3; // filter
+	//error = input;
+	errorDiff = error - errorLast;
+	erroInte = constrain(erroInte + error, -50, 50);
+	output = Kp * error + Ki * erroInte + Kd * errorDiff;
+	/*
+	Serial.print(error); Serial.print(' ');
+	Serial.print(erroInte); Serial.print(' ');
+	Serial.print(errorDiff); Serial.print(' ');
+	Serial.println(output);
+	*/
+	errorLast = error;
+
+	return output;
+}
+
+int echoTrace()
+{
 	sensor[0] = analogRead(A2);
 	sensor[1] = analogRead(A1);
 	sensor[2] = analogRead(A0);
-	
+	int ret = 0;
+	int a[3];
+	for (int i = 0; i < 3; i++)
+	{
+		a[i] = constrain((1025 - analogRead(A2 - i)) / 10 - 4, 0, 20);
+		if (a[i] > sensor[i])
+			ret += (0x1 << i);
+	}
+	return ret;
+}
+
+int bias = 0;
+int outlineCnt = 0;
+
+void stateMachine(int a)
+{
+	switch (a)
+	{
+	case B00000:
+		outlineCnt++;
+		break;
+	case B11111:
+		outlineCnt++;
+		break;
+	case B00010:
+	case B00110:
+		outlineCnt = 0;
+		bias = 1;
+		break;
+	case B00001:
+	case B00011:
+		outlineCnt = 0;
+		bias = 2;
+		break;
+	case B00100:
+		outlineCnt = 0;
+		bias = 0;
+		break;
+	case B01000:
+	case B01100:
+		outlineCnt = 0;
+		bias = -1;
+		break;
+	case B10000:
+	case B11000:
+		outlineCnt = 0;
+		bias = -2;
+		break;
+	default:
+		Serial.println(a, BIN);
+		outlineCnt++;
+		break;
+	}
+
+	if (outlineCnt > 10)
+	{
+		doDcSpeed(0, 0);
+	}
+	else
+	{
+		float ff = 150;
+		float ctrl = calcPid(bias);
+		doDcSpeed(ff - ctrl, ff + ctrl);
+	}
+}
+
+void doDcSpeed(int spdL, int spdR)
+{
+	spdR = -spdR;
+	if (spdL < 0)
+	{
+		analogWrite(5, 0);
+		analogWrite(6, -spdL);
+	}
+	else
+	{
+		analogWrite(5, spdL);
+		analogWrite(6, 0);
+	}
+
+	if (spdR < 0)
+	{
+		analogWrite(9, 0);
+		analogWrite(10, -spdR);
+	}
+	else
+	{
+		analogWrite(9, spdR);
+		analogWrite(10, 0);
+	}
 }
 
 /**
@@ -104,7 +225,8 @@ void keysacn()
 */
 void Clear_All_PWM()
 {
-	for(int i = 0; i < 16; i++){
+	for (int i = 0; i < 16; i++)
+	{
 		pwm.setPWM(i, 0, 0);
 	}
 }
---
+++ Tracking_PID_v1/Tracking_PID_v1.ino
@@ -0,0 +1,207 @@
+#include <Adafruit_PWMServoDriver.h>
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
+
+float Kp = 5, Ki = 0, Kd = 0;
+float error = 0, P = 0, I = 0, D = 0, PID_value = 0;
+float previous_error = 0, previous_I = 0;
+int sensor[3] = {0, 0, 0};
+int initial_motor_speed = 35;
+
+const int key = 7; //按键key
+
+void read_sensor_values(void);
+void calculate_pid(void);
+void motor_control(void);
+void keyscan(void);
+void Clear_All_PWM(void);
+
+void setup()
+{
+	// put your setup code here, to run once:
+	//初始化电机驱动IO为输出方式
+	pwm.begin();
+	pwm.setPWMFreq(60); // Analog servos run at ~60 Hz updates
+	Clear_All_PWM();
+
+	pinMode(A0, INPUT);
+	pinMode(A1, INPUT);
+	pinMode(A2, INPUT);
+	pinMode(key, INPUT); //定义按键输入脚
+	keysacn();
+}
+
+void loop()
+{
+	read_sensor_values();
+	calculate_pid();
+	motor_control();
+}
+
+void read_sensor_values()
+{
+	sensor[0] = digitalRead(A2);
+	sensor[1] = digitalRead(A1);
+	sensor[2] = digitalRead(A1);
+
+	// if ((sensor[0] == 0) && (sensor[1] == 0) && (sensor[2] == 0) && (sensor[4] == 0) && (sensor[4] == 1))
+	// 	error = 4;
+	// else if ((sensor[0] == 0) && (sensor[1] == 0) && (sensor[2] == 0) && (sensor[4] == 1) && (sensor[4] == 1))
+	// 	error = 3;
+	// else if ((sensor[0] == 0) && (sensor[1] == 0) && (sensor[2] == 0) && (sensor[4] == 1) && (sensor[4] == 0))
+	// 	error = 2;
+	// else if ((sensor[0] == 0) && (sensor[1] == 0) && (sensor[2] == 1) && (sensor[4] == 1) && (sensor[4] == 0))
+	// 	error = 1;
+	// else if ((sensor[0] == 0) && (sensor[1] == 0) && (sensor[2] == 1) && (sensor[4] == 0) && (sensor[4] == 0))
+	// 	error = 0;
+	// else if ((sensor[0] == 0) && (sensor[1] == 1) && (sensor[2] == 1) && (sensor[4] == 0) && (sensor[4] == 0))
+	// 	error = -1;
+	// else if ((sensor[0] == 0) && (sensor[1] == 1) && (sensor[2] == 0) && (sensor[4] == 0) && (sensor[4] == 0))
+	// 	error = -2;
+	// else if ((sensor[0] == 1) && (sensor[1] == 1) && (sensor[2] == 0) && (sensor[4] == 0) && (sensor[4] == 0))
+	// 	error = -3;
+	// else if ((sensor[0] == 1) && (sensor[1] == 0) && (sensor[2] == 0) && (sensor[4] == 0) && (sensor[4] == 0))
+	// 	error = -4;
+	// else if ((sensor[0] == 0) && (sensor[1] == 0) && (sensor[2] == 0) && (sensor[4] == 0) && (sensor[4] == 0))
+	// 	if (error == -4)
+	// 		error = -5;
+	// 	else
+	// 		error = 5;
+
+	if ((sensor[0] == 0) && (sensor[1] == 0) && (sensor[2] == 1))
+	{
+		error = 2;
+	}
+	else if ((sensor[0] == 0) && (sensor[1] == 1) && (sensor[2] == 1))
+	{
+		error = 1;
+	}
+	else if ((sensor[0] == 0) && (sensor[1] == 1) && (sensor[2] == 0))
+	{
+		error = 0;
+	}
+	else if ((sensor[0] == 1) && (sensor[1] == 1) && (sensor[2] == 0))
+	{
+		error = -1;
+	}
+	else if ((sensor[0] == 1) && (sensor[1] == 0) && (sensor[2] == 0))
+	{
+		error = -2;
+	}
+	else if ((sensor[0] == 0) && (sensor[1] == 0) && (sensor[2] == 0))
+	{
+		if (error == -2)
+		{
+			error = -3;
+		}
+		else
+		{
+			error = 3;
+		}
+	}
+}
+
+void calculate_pid()
+{
+	P = error;
+	I = I + previous_I;
+	D = error - previous_error;
+
+	PID_value = (Kp * P) + (Ki * I) + (Kd * D);
+
+	previous_I = I;
+	previous_error = error;
+}
+
+void motor_control()
+{
+	// Calculating the effective motor speed:
+	int left_motor_speed = initial_motor_speed - PID_value;
+	int right_motor_speed = initial_motor_speed + PID_value;
+
+	// The motor speed should not exceed the max PWM value
+	left_motor_speed = constrain(left_motor_speed, 0, 255);
+	right_motor_speed = constrain(right_motor_speed, 0, 255);
+
+	// analogWrite(9, initial_motor_speed - PID_value);  //Left Motor Speed
+	// analogWrite(10, initial_motor_speed + PID_value); //Right Motor Speed
+	run(left_motor_speed, right_motor_speed);
+
+	//following lines of code are to make the bot move forward
+	/*The pin numbers and high, low values might be different
+    depending on your connections */
+	// digitalWrite(4, HIGH);
+	// digitalWrite(5, LOW);
+	// digitalWrite(6, LOW);
+	// digitalWrite(7, HIGH);
+}
+
+/**
+* Function       run
+* @author        wusicaijuan
+* @date          2019.06.25
+* @brief         小车前进
+* @param[in]     Speed
+* @param[out]    void
+* @retval        void
+* @par History   无
+*/
+void run(float Speed1, float Speed2)
+{
+	Speed1 = map(Speed1, 0, 255, 0, 4095);
+	Speed2 = map(Speed2, 0, 255, 0, 4095);
+	pwm.setPWM(10, 0, Speed2); //右前
+	pwm.setPWM(11, 0, 0);
+	pwm.setPWM(8, 0, Speed2); //右后
+	pwm.setPWM(9, 0, 0);
+
+	pwm.setPWM(13, 0, Speed1); //左前
+	pwm.setPWM(12, 0, 0);
+	pwm.setPWM(15, 0, Speed1); //左后
+	pwm.setPWM(14, 0, 0);
+}
+
+/**
+* Function       keysacn
+* @author        wusicaijuan
+* @date          2019.06.04
+* @brief         按键扫描
+* @param[in1]    void
+* @retval        void
+* @par History   无
+*/
+void keysacn()
+{
+	int val;
+	val = digitalRead(key); //读取数字7口电平值赋给val
+	while (val == HIGH)		//当按键没被按下时，一直循环
+	{
+		val = digitalRead(key); //此句可省略，可让循环跑空
+	}
+	while (val == LOW) //当按键被按下时
+	{
+		delay(1);				//延时10ms
+		val = digitalRead(key); //读取数字7口电平值赋给val
+		while (val == HIGH)		//判断按键是否被松开
+		{
+			break;
+		}
+	}
+}
+
+/*
+* Function       Clear_All_PWM
+* @author        wusicaijuan
+* @date          2019.07.04
+* @brief         关闭所有PWM
+* @param[in]     void
+* @param[out]    void
+* @retval        void
+* @par History   无
+*/
+void Clear_All_PWM()
+{
+	for (int i = 0; i < 16; i++)
+	{
+		pwm.setPWM(i, 0, 0);
+	}
+}
